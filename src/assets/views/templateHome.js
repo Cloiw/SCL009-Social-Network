fcec5ac13c5a22e0c3d6b675e0614102527dd551
TatianaCastroLizama <tcl.contacto@gmail.com>
2019-06-02 00:35:13 -0400

    primera pantalla aún no resposive

--- src/assets/views/templateHome.js
+++ src/assets/views/templateHome.js
@@ -3,8 +3,15 @@ export const templateHome = () => {
     document.getElementById("root").innerHTML =
     `<div>
     <div class="logo">  <img src="assets/img/logo_full.png" alt=""> </div>
-    <p>La red social para amantes blabla</p>  
-    <button class="btn-google-login" id="login">Login con Google</button> 
+    <p>La red social para amantes del Teatro</p>  
+    <hr>
+    <input class="email" type="email" id="email" placeholder="Correo">
+    <input class="password" type="password" id="password" placeholder="Contraseña"> <br>
+    <button class="btn-login" id="login">Log In</button> 
+    <button class="btn-google-login" id="login">Entrar con google</button> 
+    <hr>
+    <p>¿No tienes una cuenta?<p>
+    <button class="btn-register" id="login">Registrarse</button> 
     </div>`
 
 
