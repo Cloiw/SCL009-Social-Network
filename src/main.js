4d300fab8878852fc21353b212e8cfc01df0830d
TatianaCastroLizama <tcl.contacto@gmail.com>
2019-06-01 20:14:09 -0400

    empezando primera pantalla

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
 
 import { initFirebase } from './assets/js/initFirebase.js';
 
+import {initRouter} from './route.js';
+import {initFirebase} from './assets/js/initFirebase.js'
+
 const init = () => {
     initFirebase();
 }
 
+
+const init =()=>{
+    initRouter();
+}
 
 window.addEventListener('load', init);
