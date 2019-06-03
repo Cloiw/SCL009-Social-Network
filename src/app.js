5ffab48e941cc3ce75e3f679b578591b7747500e
TatianaCastroLizama <tcl.contacto@gmail.com>
2019-06-03 14:59:15 -0400

    observer

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 //Archivo base que comunica a todas las cosas
 
-import {
-    initFirebase
-} from './assets/js/initFirebase.js';
+import {initFirebase} from './assets/js/initFirebase.js';
 
-import {
-    initRouter
-} from './route.js';
+import {initRouter} from './route.js';
+import { observer } from './assets/js/auth.js'
 
 
 const init = () => {
 
     initRouter();
     initFirebase();
+    observer();
 }
 
 window.addEventListener('load', init);
