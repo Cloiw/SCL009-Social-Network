e92d1b2336f0fb5014d573a089dc4e3d6158308d
Cloiw <sircli@hotmail.com>
2019-06-06 12:22:56 -0400

    Agregando verificacion de correo en signIn

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -1,4 +1,6 @@
 
+
+//Logeao con Google
 export const loginGoogle = () =>{
     let provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -19,8 +21,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
  });}
 
 
- /*Función que envia un correo de validación de la cuenta 
-una vez que el usuario se registra satisfactoriamente*/
+ //Envia un correo de validación de la cuenta una vez que el usuario se registra satisfactoriamente
 function emailVerification() {
   let user = firebase.auth().currentUser;
 
@@ -32,14 +33,14 @@ function emailVerification() {
   })
 }
 
+//Crea una cuenta y guarda los datos. Envia un correo de verificación luego del registro.
 export const createAccount = (userName, userAge,userLocation, userEmail, userPassword) => {
   let dbProfiles = firebase.firestore();
-  /*Función de Firebase para registrar nuevos usuarios*/
+  // Función de Firebase para registrar nuevos usuarios
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
       .then(function () {
         emailVerification();
-        /*Base de datos, para almacenar de manera paralela en cloud firestore 
-        dichos datos del usuario*/
+        //Base de datos, para almacenar de manera paralela en cloud firestore  dichos datos del usuario
         dbProfiles.collection("users").add({
           name: userName,
           age: userAge,
@@ -48,45 +49,50 @@ export const createAccount = (userName, userAge,userLocation, userEmail, userPas
         })
           .then(function () {
             console.log("Document successfully written!");
-            /*Llama a la función emailVerification()*/
-          
             alert("El usuario ha sido creado con éxito. Hemos enviado un correo de verificación de cuenta.");
-            /*Cambia la vista de usuario de templateCreate a templateSignIn, a partir del hash*/
             window.location.hash = '';
           })
           .catch(function (error) {
-            /*Si no ocurre el regitro, imprime un mensaje de error*/
-            console.error("Error writing document: ", error);
-            return "Error escribiendo perfil en firebase";
+            //Si no ocurre el regitro, imprime un mensaje de error
+            console.error("Error writing document: ", error);  
           });
       })
       .catch(function (error) {
         if (error) {
-          /*Si no sucede el registro, envia menseaje de error al usuario*/
+          //Si no sucede el registro, envia menseaje de error al usuario
           alert('No se ha podido crear el usuario');
-          return 'No se ha podido crear el usuario';
+      
         }
       });
   }
 
-  /* Función para realizar login usando un la cuenta creada*/
 
+  //Permite iniciar sesión solo a usuarios que hayan verificado su correo
 export const signIn = (emailSignIn, passwordSignIn) => {
      document.getElementById("error-fb").innerHTML=""
  
-    /*Función firebase para ingreso de usuarios registrados*/
+    //Función firebase para ingreso de usuarios registrados
     firebase.auth().signInWithEmailAndPassword(emailSignIn, passwordSignIn)
       .then(function () {
-        alert("Has iniciado sesión con exito");
-        window.location.hash = '#/wall';
+        firebase.auth().onAuthStateChanged(user => {
+          if(!user.emailVerified){
+            console.log(user.emailVerified)
+            window.location.hash = '';
+            document.getElementById("error-fb").innerHTML= `Verifica tu correo para poder iniciar sesión`;
+          }
+          if(user.emailVerified){
+            console.log(user.emailVerified)
+            window.location.hash = '#/wall';
+          }
+        }) 
       })
       .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        document.getElementById("error-fb").innerHTML= `${errorMessage}`;
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          document.getElementById("error-fb").innerHTML= `${errorMessage}`;
+          console.log(errorCode);
       });
-  }
+}
 
 //  /*Función Observador, que verifica que el usuario se encuentra logueado*/
 // export const observer = () => {
@@ -105,6 +111,33 @@ export const signIn = (emailSignIn, passwordSignIn) => {
 //     }
 //   });
 // }
+
+// export const observer= () =>{
+// firebase.auth().onAuthStateChanged(user => {
+//     if (user && user.emailVerified()) {
+//       window.location.hash = '#/wall';
+//       ;
+//     } else {
+//       console.log('no existe usuario activo');
+//       window.location.hash = ""
+      
+//     }
+// })}
+
+
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//   } else {
+//     // No user is signed in.
+//   }
+// });
+
+
+// firebase.auth().currentUser;
+// if (user.emailVerified()) {
+  
+  
   
 
 
