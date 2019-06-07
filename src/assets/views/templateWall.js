805823a3196a4089fb231fc3153be6739e82c372
Cloiw <sircli@hotmail.com>
2019-06-07 17:52:42 -0400

    Modificado CSS templateWall

--- src/assets/views/templateWall.js
+++ src/assets/views/templateWall.js
@@ -4,13 +4,29 @@ export const templateWall = () => {
     observer()
     document.getElementById("root").innerHTML =
         
-    `<div class="container-nav">
-    <nav id="nav-wall" 
-        <div class="signOut">
-        <button id="btn-signout">
-        <img src="assets/img/logout.png" id="logout" alt="Boton salir aplicación"/></button> 
-        </div>  
-    </nav>
+    `
+    <div class="container-row">
+        <div class="left-col">
+            <div class="btn-nav">
+                <div class="icon-btn">
+                <img class="logo-icon"  src="assets/img/logo_small.png"></div>
+            </div>
+        </div>
+
+        <div class="center-col">
+            <div class="icons-row">
+                <img id="btn-profile" class="btn-icons" src="assets/img/profile_icon.png">
+                <img id="btn-wall" class="btn-icons" src="assets/img/wall_icon.png">
+                <img id="btn-post" class="btn-icons" src="assets/img/post_icon.png">
+                <button id="logout-btn" class="icon-btn">
+                    <img id="btn-post" class="btn-icons" src="assets/img/logout_icon.png">
+                </button>
+            </div>
+            
+        </div>
+
+        
+         </div>
     </div>
     <section class="template-wall">
                             <h2>Bienvenido</h2>
@@ -29,7 +45,7 @@ export const templateWall = () => {
                         `
 
     //evento para cerrar la sesión del usuario
-    document.getElementById('logout').addEventListener('click', () => {
+    document.getElementById('logout-btn').addEventListener('click', () => {
         signOut();
     })
     //evento que sirve para volver al inicio de la pantalla
@@ -37,3 +53,6 @@ export const templateWall = () => {
     //    window.scrollTo(0,0);
     //});    
 }
+
+
+//<img src="assets/img/logout.png" id="logout" alt="Boton salir aplicación"/>
